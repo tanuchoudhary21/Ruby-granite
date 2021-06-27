73d07121c75c8cce18e12004a4d46173237cfa2c
tanuchoudhary21 <chtaruna90@gmail.com>
2021-06-27 20:33:17 +0530

    Added tasks controller and index action

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -19,24 +19,6 @@ const App = () => {
       </Switch>
     </Router>
   );
-
-  // previous code without changes
 };
 
-// import React, { useEffect, useState } from "react";
-// import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
-
-// const App = () => {
-//   const [loading, setLoading] = useState(true);
-
-//   return (
-//     <Router>
-//       <Switch>
-//         <Route exact path="/" render={() => <div>Home</div>} />
-//         <Route exact path="/about" render={() => <div>About</div>} />
-//       </Switch>
-//     </Router>
-//   );
-// };
-
-// export default App;
+export default App;
