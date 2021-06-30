ad47dc55c9c0de94b2ec4261b0f9e783910444f2
tanuchoudhary21 <chtaruna90@gmail.com>
2021-06-30 17:26:49 +0530

    Added login functionality with displaying names

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,15 @@
 class SessionsController < ApplicationController
-    def create
-     user = User.find_by(email: login_params[:email].downcase)
-     if user.present? && user.authenticate(login_params[:password])
-       render status: :ok, json: { auth_token: user.authentication_token, userId: user.id }
-     else
+
+  def create
+    user = User.find_by(email: login_params[:email].downcase)
+    if user.present? && user.authenticate(login_params[:password])
+      render status: :ok, json: { auth_token: user.authentication_token,
+                                  userId: user.id,
+                                  user_name: user.name }
+    else
       render status: :unauthorized, json: { notice: t('session.incorrect_credentials') }
-     end
     end
+  end
  
    private
  
