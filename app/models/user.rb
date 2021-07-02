c23902379063b6467aee8f029a0eb919894d0713
tanuchoudhary21 <chtaruna90@gmail.com>
2021-07-02 09:38:40 +0530

    Added email validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,20 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
+  validates :email, presence: true,
+                    uniqueness: true,
+                    length: { maximum: 50 },
+                    format: { with: VALID_EMAIL_REGEX }
+
 
   validates :name, presence: true, length: { maximum: 35 }
+  validates :password, presence: true, confirmation: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true, on: :create
+  
   has_many :tasks, dependent: :destroy, foreign_key: :user_id
   has_many :comments, dependent: :destroy
   has_secure_password
   has_secure_token :authentication_token
 
-  validates :email, presence: true,
-                    uniqueness: true,
-                    length: { maximum: 50 },
-                    format: { with: VALID_EMAIL_REGEX }
-  validates :password, presence: true, confirmation: true, length: { minimum: 6 }
-  validates :password_confirmation, presence: true, on: :create
 
   before_save :to_lowercase
 
