47a1b28f2ffaba1e538631a623df7ad54cdc8d6e
tanuchoudhary21 <chtaruna90@gmail.com>
2021-07-04 13:18:13 +0530

    Added sidekiq and backend task_logger_job with modifications

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,45 +1,41 @@
 class Task < ApplicationRecord
-  
-    validates :title, presence: true, length: { maximum: 50 }
-    belongs_to :user
-    has_many :comments, dependent: :destroy
-    enum progress: { pending: 0, completed: 1 }
-    enum status: { unstarred: 0, starred: 1 }
-    validates :slug, uniqueness: true
-    before_create :set_slug
-    validate :slug_not_changed
-    after_create :log_task_details
-    
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :slug, uniqueness: true
+  before_create :set_slug
+  after_create :log_task_details
+  validate :slug_not_changed
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+  enum progress: { pending: 0, completed: 1 }
+  enum status: {unstarred:0, starred:1}
 
-    private
+  private
 
-    def self.organize(progress)
-      starred = send(progress).starred.order('updated_at DESC')
-      unstarred = send(progress).unstarred
-      starred + unstarred
+  def set_slug
+    itr = 1
+    loop do
+      title_slug = title.parameterize
+      slug_candidate = itr > 1 ? "#{title_slug}-#{itr}" : title_slug
+      break self.slug = slug_candidate unless Task.exists?(slug: slug_candidate)
+      itr += 1
     end
-
-    private
-
-    def set_slug
-      itr = 1
-      loop do
-        title_slug = title.parameterize
-        slug_candidate = itr > 1 ? "#{title_slug}-#{itr}" : title_slug
-        break self.slug = slug_candidate unless Task.exists?(slug: slug_candidate)
-        itr += 1
-      end
-    end
-  
-    def slug_not_changed
-      if slug_changed? && self.persisted?
-        errors.add(:slug, t('task.slug.immutable'))
-      end
-    end
-
-    def log_task_details
-      TaskLoggerJob.perform_later(self)
-    end
-  
   end
-  
+
+  def slug_not_changed
+    if slug_changed? && self.persisted?
+      errors.add(:slug, t('task.slug.immutable'))
+    end
+  end
+
+  def self.organize(progress)
+    # send(progress) means tasks.send(progress) 
+    starred = send(progress).starred.order('updated_at DESC')
+    unstarred = send(progress).unstarred
+    starred + unstarred
+  end
+
+  def log_task_details
+    TaskLoggerJob.perform_later(self)
+  end
+
+end
