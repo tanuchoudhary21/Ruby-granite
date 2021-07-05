96aff0bba0f091ad7adf0ed5303fa655849be025
tanuchoudhary21 <chtaruna90@gmail.com>
2021-07-05 13:23:31 +0530

    Added test cases for sidekiq email sending feature

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative "../config/environment"
 require "rails/test_help"
+Dir[Rails.root.join("test/support/**/*.rb")].each { |f| require f }
 
 class ActiveSupport::TestCase
   include ActionView::Helpers::TranslationHelper
